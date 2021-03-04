0d444adb4b47f05e00b8b1846d14f0dc6569cc47
Mark Bower <mark.bower@yale.edu>
2021-03-04 06:54:04 -0700

    Eliminated name class by SingletonInR

--- Analysis/SingletonsInR/R/SingletonInR.R
+++ Analysis/SingletonsInR/R/SingletonInR.R
@@ -2,7 +2,7 @@ library( R6 )
 library( R6P )
 
 #' @export
-Singleton <- R6::R6Class("Singleton", inherit = R6P::Singleton, public = list(
+SingletonInR <- R6::R6Class("Singleton", inherit = R6P::Singleton, public = list(
   input = NA,
   value = ifelse( exists("self$value"), self$value, NA ),
   assign = function(input) {self$value = input}
